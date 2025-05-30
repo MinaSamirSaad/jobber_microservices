b9575ee9f232f33672e6f7d4cf88b04d14234de3
MinaSamirSaad <minasamir.nashid@gmail.com>
2025-05-30 22:46:20 +0300

    feat: integrate ConfigModule and update service tests for improved configuration management

--- apps/jobber-auth/src/app/users/users.resolver.spec.ts
+++ apps/jobber-auth/src/app/users/users.resolver.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
+import { PrismaService } from '../prisma/prisma.service';
+import { UsersService } from './users.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
+      providers: [
+        UsersResolver,
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
